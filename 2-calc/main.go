aa86e1d68dbbd78ca47fa150b0e134c6642d2ffd
bondarenko.o <bondarenko_oleg@list.ru>
2025-10-23 16:09:56 +0300

    ДЗ 6.15

---
+++ 2-calc/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	operation := getUserInputOperation()
+	numbers := getUserInputNumber()
+
+	result := operationWithNumbers(numbers, operation)
+
+	fmt.Println(result)
+}
+
+func getUserInputOperation() string {
+	var operaition string
+	for {
+		fmt.Print("Введите необходимую операцию (AVG - среднее, SUM - сумму, MED - медиану): ")
+		fmt.Scan(&operaition)
+
+		_, err := checkOperation(operaition)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		return strings.ToUpper(operaition)
+	}
+}
+
+func checkOperation(operaition string) (bool, error) {
+	if strings.ToUpper(operaition) == "AVG" || strings.ToUpper(operaition) == "SUM" || strings.ToUpper(operaition) == "MED" {
+		return true, nil
+	}
+
+	return false, errors.New("Введена недоступная операция. Повторите ввод")
+}
+
+func getUserInputNumber() string {
+	var numbers string
+	fmt.Print("Введите числа через запятую:")
+	fmt.Scan(&numbers)
+
+	return numbers
+}
+
+func operationWithNumbers(numbers string, operation string) float64 {
+	arrNumbers := strings.Split(numbers, ",")
+	var arrForCalc []float64
+	var result float64
+
+	for _, value := range arrNumbers {
+		number, _ := strconv.ParseFloat(strings.TrimSpace(value), 64)
+		arrForCalc = append(arrForCalc, number)
+	}
+
+	switch {
+	case operation == "AVG":
+		result = calculateAvg(arrForCalc)
+	case operation == "SUM":
+		result = calculateSum(arrForCalc)
+	case operation == "MED":
+		result = calculateMed(arrForCalc)
+	}
+
+	return result
+}
+
+func calculateAvg(numbers []float64) float64 {
+	result := 0.0
+
+	for _, value := range numbers {
+		result += value
+	}
+
+	return result / float64(len(numbers))
+}
+
+func calculateSum(numbers []float64) float64 {
+	result := 0.0
+
+	for _, value := range numbers {
+		result += value
+	}
+	return result
+}
+
+func calculateMed(numbers []float64) float64 {
+	arrLenght := len(numbers)
+
+	if arrLenght%2 == 1 {
+		return numbers[arrLenght/2]
+	}
+
+	return (numbers[arrLenght/2-1] + numbers[arrLenght/2]) / 2
+}
