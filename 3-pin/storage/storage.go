28a5859a595a29f9e69248a75a6ef186b7586e87
bondarenko.o <bondarenko_oleg@list.ru>
2025-11-12 19:09:16 +0300

    ДЗ 11.16

--- 3-pin/storage/storage.go
+++ 3-pin/storage/storage.go
@@ -1,1 +1,36 @@
 package storage
+
+import (
+	"bin/bins"
+	"bin/file"
+	"encoding/json"
+	"fmt"
+)
+
+func saveBin(bin *bins.Bin) {
+	data, err := json.Marshal(bin)
+
+	if err != nil {
+		fmt.Println("Не удалось преобразовать в JSON")
+		return
+	}
+
+	err = file.WriteJson(data, "data.json")
+	if err != nil {
+		return
+	}
+}
+
+func readBin(fileName string) (*bins.Bin, error) {
+	data, err := file.ReadJson(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	var bin bins.Bin
+	err = json.Unmarshal(data, &bin)
+	if err != nil {
+		return nil, err
+	}
+	return &bin, nil
+}
